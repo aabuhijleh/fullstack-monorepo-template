df84b900c13ae50ff0c8ba9d4a21a32fad006fe6
aabuhijleh <aabuhijleh.dev@gmail.com>
2026-05-27 12:39:33 +0300

    feat: add skills

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,8 +1,13 @@
-import { defineSchema } from "convex/server"
+import { defineSchema, defineTable } from "convex/server"
+import { v } from "convex/values"
 import { authTables } from "@convex-dev/auth/server"
 
 const schema = defineSchema({
   ...authTables,
+  tasks: defineTable({
+    text: v.string(),
+    isCompleted: v.boolean(),
+  }),
 })
 
 export default schema
--- packages/backend/convex/tasks.ts
+++ packages/backend/convex/tasks.ts
@@ -1,8 +1,13 @@
+import { getAuthUserId } from "@convex-dev/auth/server"
 import { query } from "./_generated/server"
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx)
+    if (userId === null) {
+      throw new Error("Unauthorized")
+    }
     return await ctx.db.query("tasks").collect()
   },
 })
