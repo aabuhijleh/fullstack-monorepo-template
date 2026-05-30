c29ce4175bd906b771ed289aa880c4c5d8e71051
aabuhijleh <aabuhijleh.dev@gmail.com>
2026-05-30 13:15:27 +0300

    feat: add task components to mobile

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,6 +1,10 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: "jest-expo",
+  moduleNameMapper: {
+    "^react$": require.resolve("react"),
+    "^react/(.*)$": "react/$1",
+  },
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|nativewind|react-native-css))",
   ],
